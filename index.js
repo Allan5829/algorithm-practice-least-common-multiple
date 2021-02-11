1fdb13019bf0f50f3f6949d8cea9dfb267a4f380
Allan5829 <allan.farca@gmail.com>
2021-02-11 01:33:30 -0800

    adds comments to explain code and adds more examples.

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ function lcm(nums) {
                     factors.push(x)
                 }
             } else {
+                // Example [ 2, 5, 5 ], [ 2, 2, 2, 5 ]
+                // This bit of code compares if a value is included in factors, is it the same number of times as in the allFactors sub array.
+                // If not push the x value a number of times equal to the difference between counts.
                 let currentCount = calculateCount(factors, x)
                 let newCount = calculateCount(allFactors[i], x)
                 if (currentCount !== newCount) {
@@ -39,6 +42,7 @@ function lcm(nums) {
         })
     }
 
+    // Multiply every value together to get the least common multiple.
     let result = factors.reduce((total, x) => {
         return total * x
     })
@@ -47,6 +51,7 @@ function lcm(nums) {
     return result
 }
 
+// Given an array and a value this function returns how many times a number is present within the argument array.
 function calculateCount(array, num) {
     let count = 0;
     for (let i = 0; i < array.length; i++) {
@@ -55,11 +60,12 @@ function calculateCount(array, num) {
     return count
 }
 
+// Returns an array of prime numbers.
 function prime_factors(num) {
     const primeFactors = [];
     if (num === 1) primeFactors.push(num);
     for (let i = 2; i <= num; i++) { 
-      while (is_prime(i) && num % i === 0) // if i is prime and remainder of num/i is 0
+      while (is_prime(i) && num % i === 0)
       {
         primeFactors.push(i)
         num /= i;
@@ -68,7 +74,8 @@ function prime_factors(num) {
     return primeFactors;
 }
 
-function is_prime(num) { //checks if a number is prime by comparing the argument's square root
+// Checks if a number is prime by comparing the argument's square root within a loop and returning true/false.
+function is_prime(num) { 
     for (let i = 2; i <= Math.sqrt(num); i++)
     {
       if (num % i === 0) return false;
@@ -84,4 +91,7 @@ lcm([1, 2, 3, 4, 5, 6, 7, 8, 9]) ➞ 2520
 lcm([5]) ➞ 5
 lcm([5, 7, 11]) ➞ 385
 lcm([5, 7, 11, 35, 55, 77]) ➞ 385
+lcm([79, 18, 7, 3, 1]) => 9954
+lcm([10, 20, 30, 40, 50]) => 600
+lcm([2, 3, 5, 7, 11, 13, 17]) => 510510
 */
