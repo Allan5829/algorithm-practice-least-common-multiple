8dd55178758320fc1981ecc34f86631fe0931942
Allan5829 <allan.farca@gmail.com>
2021-02-11 01:15:41 -0800

    writes comments explaining code and discovers flaw in code.

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 function lcm(nums) {
 
+    // Sort argument from big to small.
     let sorted = nums.sort(function(a, b) {
-        return a - b;
+        return b - a;
     });
 
+    // Turns arguments into prime numbers and organizes data in [ [], [] ] format
     let allFactors = sorted.map( x => {
         return prime_factors(x)
     })
-    
-    //console.log(allFactors)
 
+    console.log(allFactors)
     let factors = [];
+
+    // If argument is 1 value and a prime number
     if (allFactors.length === 1) {
         factors.push( allFactors.flat()[0] )
     }
-    for (let i = 0; 1 < allFactors.length; allFactors.pop()){
-        i = allFactors.length - 1
+
+    // Loop is responsible for populating factors array.
+    // Only pushes least amount of needed factors for least common multiple.
+    // Need to edit to account for all prime numbers.
+    for (let i = 0; 1 < allFactors.length; allFactors.shift()){
         allFactors[i].forEach( x => {
             if (!factors.includes(x)) { // if the current factor isn't included in factors do stuff
                 let repeat = calculateCount(allFactors[i], x) // get how many times the current number is in the array
@@ -30,20 +36,21 @@ function lcm(nums) {
         return total * x
     })
 
-    // check if the lcm is the actual lcm and if not multiply by 2 until it is because that was the common theme I saw
-    let lcm = 0;
-    function checkLcm(array, num) {
+    let result = 0;
+    // Checks if the currentLcm has any remainders greater than 0 when divided by each of the indvidual arguments.
+    // Placed function inside so it has access to a variable that will be our result.
+    function checkLcm(array, num) { 
         let check = array.every( x => {return num%x === 0})
         if(check) {
-            lcm = num
+            result = num
         } else {
             checkLcm(array, num*2)
         }
     }
     checkLcm(sorted, currentLcm)
 
-    console.log(lcm)
-    return lcm
+    console.log(result)
+    return result
 }
 
 function calculateCount(array, num) {
@@ -58,7 +65,7 @@ function prime_factors(num) {
     const primeFactors = [];
     if (num === 1) primeFactors.push(num);
     for (let i = 2; i <= num; i++) { 
-      while (is_prime(i) && num % i === 0) // if i is prime and num/i is a whole number
+      while (is_prime(i) && num % i === 0) // if i is prime and remainder of num/i is 0
       {
         primeFactors.push(i)
         num /= i;
@@ -75,7 +82,7 @@ function is_prime(num) { //checks if a number is prime by comparing the argument
     return true;
 }
 
-lcm([67, 34, 12, 3, 2])
+lcm([10, 20, 30, 40, 50])
 
 /*
 examples
