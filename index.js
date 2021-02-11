3f51d611db431eb17ea97c75292c8b7cb975c86d
Allan5829 <allan.farca@gmail.com>
2021-02-10 22:58:50 -0800

    set up index file and comments pseduo code and examples.

---
+++ index.js
@@ -0,0 +1,15 @@
+function lcm(nums) {
+	
+}
+
+/*
+psuedo code
+find prime numbers for current index and multiply for least common multiple
+
+examples
+lcm([1, 2, 3, 4, 5, 6, 7, 8, 9]) ➞ 2520
+lcm([5]) ➞ 5
+lcm([5, 7, 11]) ➞ 385
+lcm([5, 7, 11, 35, 55, 77]) ➞ 385
+
+*/
